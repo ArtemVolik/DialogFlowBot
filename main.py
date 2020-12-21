6114ece020c0ffc39496cc567cdfda4883aeff23
artyom <art.volyk@gmail.com>
2020-12-22 00:49:50 +0200

    handling TG bot exception through native dispatcher

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 import environ
 from telegram.ext import Updater, MessageHandler, Filters
-from telegram import Bot
+from telegram import Bot, TelegramError
 from integrations.google_dialogflow import get_agent_answer
-import logging.config
-from logs.logging_maintenace import logging_config_loading
+from logs.logging_maintenace import get_logger_from_config
 
 
 def update_handling(update, context):
-    get_google_bot_answer = get_agent_answer(project_id, update.effective_chat.id, update.message.text,
-                                                language_code)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=get_google_bot_answer.fulfillment_text)
+    get_google_bot_answer = get_agent_answer(
+        project_id,
+        update.effective_chat.id,
+        update.message.text,
+        language_code)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=get_google_bot_answer.fulfillment_text)
+
+
+def error_callback(context):
+    try:
+        raise context.error
+    except TelegramError as er:
+        logger.exception(er)
 
 
 if __name__ == "__main__":
     env = environ.Env()
     env.read_env()
-    logging_config_loading('logs/log_config.yaml', env('NOTIFICATION_TOKEN'), env('NOTIFICATION_CHAT_ID'))
-    logger = logging.getLogger("DialogBot")
+    logger = get_logger_from_config(
+        'logs/log_config.yaml', env('NOTIFICATION_TOKEN'),
+        env('NOTIFICATION_CHAT_ID'), "DialogBot")
     logger.warning('Telegram Dialog Bot launched')
-
-    telegram_token = env('TELEGRAM_TOKEN')
-    bot = Bot(token=telegram_token)
     project_id = env('GOOGLE_PROJECT_ID')
     language_code = env('LANGUAGE_CODE')
+    telegram_token = env('TELEGRAM_TOKEN')
+
+    bot = Bot(token=telegram_token)
     updater = Updater(token=telegram_token, use_context=True)
     dispatcher = updater.dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), update_handling)
-    dispatcher.add_handler(echo_handler)
-    while True:
-        try:
-            updater.start_polling()
-        except ConnectionError as er:
-            logger.warning(er)
-        except Exception as er:
-            logger.exception(er)
+    dialog_handler = MessageHandler(
+        Filters.text & (~Filters.command),
+        update_handling)
+    dispatcher.add_handler(dialog_handler)
+    dispatcher.add_error_handler(error_callback)
+    updater.start_polling()
